e58f02272bd48214ce82a838df79299b2f5f1167
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-17 15:32:21 -0400

    Correciones realizadas y pantalla inicial agregada

--- app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/MainActivity.kt
@@ -4,67 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
-import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
+import edu.ucne.registrotecnicos.data.repository.PrioridadesRepository
 import edu.ucne.registrotecnicos.data.repository.TecnicosRepository
+import edu.ucne.registrotecnicos.data.repository.TicketsRepository
+import edu.ucne.registrotecnicos.presentation.navigation.HomeNavHost
+import edu.ucne.registrotecnicos.presentation.navigation.PrioridadesNavHost
 import edu.ucne.registrotecnicos.presentation.navigation.TecnicosNavHost
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoListScreen
-import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicoScreen
+import edu.ucne.registrotecnicos.presentation.navigation.TicketsNavHost
+import edu.ucne.registrotecnicos.presentation.prioridades.PrioridadesViewModel
 import edu.ucne.registrotecnicos.presentation.tecnicos.TecnicosViewModel
+import edu.ucne.registrotecnicos.presentation.tickets.TicketsViewModel
 import edu.ucne.registrotecnicos.ui.theme.RegistroTecnicosTheme
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoDb: TecnicoDb
     private lateinit var tecnicosRepository: TecnicosRepository
     private lateinit var tecnicosViewModel: TecnicosViewModel
+
+    private lateinit var prioridadesRepository: PrioridadesRepository
+    private lateinit var prioridadViewModel: PrioridadesViewModel
+
+    private lateinit var ticketsRepository: TicketsRepository
+    private lateinit var ticketsViewModel: TicketsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -78,12 +51,22 @@ class MainActivity : ComponentActivity() {
             .build()
 
         tecnicosRepository = TecnicosRepository(tecnicoDb.TecnicoDao())
-
         tecnicosViewModel = TecnicosViewModel(tecnicosRepository)
+
+        prioridadesRepository = PrioridadesRepository(tecnicoDb.PrioridadDao())
+        prioridadViewModel = PrioridadesViewModel(prioridadesRepository)
+
+        ticketsRepository = TicketsRepository(tecnicoDb.TicketDao())
+        ticketsViewModel = TicketsViewModel(
+            ticketsRepository,
+            tecnicosRepository,
+            prioridadesRepository
+        )
 
         setContent {
             val lifecycleOwner = LocalLifecycleOwner.current
-            val tecnicoList by tecnicoDb.TecnicoDao().getAll()
+
+            val ticketList by tecnicoDb.TicketDao().getAll()
                 .collectAsStateWithLifecycle(
                     initialValue = emptyList(),
                     lifecycleOwner = lifecycleOwner,
@@ -92,16 +75,21 @@ class MainActivity : ComponentActivity() {
 
             RegistroTecnicosTheme {
                 val nav = rememberNavController()
-                TecnicosNavHost(
-                    nav,
-                    tecnicoList,
-                    tecnicosViewModel,
-                    nav
-                    )
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        HomeNavHost(
+                            navHostController = nav,
+                            prioridadesViewModel = prioridadViewModel,
+                            tecnicosViewModel = tecnicosViewModel,
+                            ticketsViewModel = ticketsViewModel
+                        )
+                    }
+                }
             }
         }
     }
-
-
 }
 
