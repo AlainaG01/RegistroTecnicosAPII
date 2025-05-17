6701d384e3572dea3fe8e3e2382dfd84ddfbbe7f
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-17 00:21:21 -0400

    entidades agregadas a la DB

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
@@ -2,17 +2,40 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import edu.ucne.registrotecnicos.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicoDao
+import edu.ucne.registrotecnicos.data.local.dao.TicketDao
+import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnicos.data.local.entities.TecnicoEntity
-import junit.runner.Version
+import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
 
 @Database(
     entities = [
-        TecnicoEntity::class
+        TecnicoEntity::class,
+        PrioridadEntity::class,
+        TicketEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun TecnicoDao(): TecnicoDao
+    abstract fun PrioridadDao(): PrioridadDao
+    abstract fun TicketDao(): TicketDao
 }
