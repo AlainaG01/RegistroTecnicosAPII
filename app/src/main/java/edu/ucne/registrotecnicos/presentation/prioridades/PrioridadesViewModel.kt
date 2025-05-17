30a20ecedb765c6ba7ddbcbc4fe4e7d97838ecbc
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-16 23:55:45 -0400

    ViewModel de Prioridad realizado

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/prioridades/PrioridadesViewModel.kt
@@ -0,0 +1,27 @@
+package edu.ucne.registrotecnicos.presentation.prioridades
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import edu.ucne.registrotecnicos.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnicos.data.repository.PrioridadesRepository
+import kotlinx.coroutines.launch
+
+data class PrioridadesViewModel(
+    private val prioridadesRepository: PrioridadesRepository
+): ViewModel() {
+    fun savePrioridad(prioridad: PrioridadEntity){
+        viewModelScope.launch {
+            prioridadesRepository.save(prioridad)
+        }
+    }
+
+    suspend fun findPrioridad(id: Int): PrioridadEntity? {
+        return prioridadesRepository.find(id)
+    }
+
+    fun deletePrioridad(prioridad: PrioridadEntity){
+        viewModelScope.launch {
+            prioridadesRepository.delete(prioridad)
+        }
+    }
+}
