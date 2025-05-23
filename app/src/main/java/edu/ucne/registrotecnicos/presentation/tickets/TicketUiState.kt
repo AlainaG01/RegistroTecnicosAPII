bfbea3f666975a7826ca73ace03b79b9ac81e3c5
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-23 03:11:05 -0400

    UiState creados

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/tickets/TicketUiState.kt
@@ -0,0 +1,16 @@
+package edu.ucne.registrotecnicos.presentation.tickets
+
+import edu.ucne.registrotecnicos.data.local.entities.TicketEntity
+import java.util.Date
+
+data class TicketUiState(
+    val ticketId: Int? = null,
+    val fecha: Date = Date(),
+    val prioridadId: Int,
+    val cliente: String = "",
+    val asunto: String = "",
+    val descripcion: String = "",
+    val tecnicoId: Int,
+    val errorMessage: String? = null,
+    val tickets: List<TicketEntity> = emptyList()
+)
