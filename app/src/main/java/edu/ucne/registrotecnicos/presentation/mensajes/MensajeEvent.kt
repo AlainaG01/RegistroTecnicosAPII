161e803687ac2b58149bb5d8eb073e0df5f22f7c
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-24 09:26:07 -0400

    Event de Mensajes realizado

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/mensajes/MensajeEvent.kt
@@ -0,0 +1,14 @@
+package edu.ucne.registrotecnicos.presentation.mensajes
+
+import java.util.Date
+
+sealed interface MensajeEvent {
+    data class MensajeChange(val mensajeId: Int): MensajeEvent
+    data class FechaChange(val fecha: Date): MensajeEvent
+    data class ContenidoChange(val contenido: String): MensajeEvent
+    data class RemitenteChange(val remitente: String): MensajeEvent
+    data class TickectIdChange(val tickectId: Int): MensajeEvent
+    data object Save: MensajeEvent
+    data object Delete: MensajeEvent
+    data object New: MensajeEvent
+}
