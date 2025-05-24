0ec28887f435b7cf50005472165af79c71f6181a
alainagarcia <alaina_garcia@ucne.edu.do>
2025-05-24 09:25:57 -0400

    UiState de Mensajes realizado

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/mensajes/MensajeUiState.kt
@@ -0,0 +1,14 @@
+package edu.ucne.registrotecnicos.presentation.mensajes
+
+import edu.ucne.registrotecnicos.data.local.entities.MensajeEntity
+import java.util.Date
+
+data class MensajeUiState(
+    val mensajeId: Int? = null,
+    val fecha: Date = Date(),
+    val contenido: String? = null,
+    val remitente: String? = null,
+    val ticketId: Int? = 0,
+    val errorMessage: String? = null,
+    val mensajes: List<MensajeEntity> = emptyList()
+)
