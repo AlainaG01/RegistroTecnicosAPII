b9e7995d6b6d53cb38c5328d7c7dfd687b70f1be
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-14 00:58:19 -0400

    UsuarioUistate realizado

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/usuarios/UsuarioUiState.kt
@@ -0,0 +1,15 @@
+package edu.ucne.registrotecnicos.presentation.usuarios
+
+import edu.ucne.registrotecnicos.data.remote.dto.UsuarioDto
+
+data class UsuarioUiState(
+    val usuarioId: Int? = null,
+    val nombre: String = "",
+    val balance: Double = 0.0,
+    val errorMessage: String? = null,
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false,
+    val errorNombre: String? = null,
+    val errorBalance: String? = null,
+    val usuarios: List<UsuarioDto> = emptyList()
+)
