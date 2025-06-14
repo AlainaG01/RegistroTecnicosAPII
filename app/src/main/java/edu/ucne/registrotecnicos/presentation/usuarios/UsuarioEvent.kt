1ca17a9d17de0f9be96850f211c5f98ff98cc391
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-14 00:58:48 -0400

    UsuarioEvent realizado

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/usuarios/UsuarioEvent.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registrotecnicos.presentation.usuarios
+
+sealed interface UsuarioEvent {
+    data class UsuarioIdChange(val usuarioId: Int): UsuarioEvent
+    data class NombreChange(val nombre: String): UsuarioEvent
+    data class BalanceChange(val balance: Double): UsuarioEvent
+
+    data object PostUsuario: UsuarioEvent
+    data object GetUsuarios: UsuarioEvent
+    data object Nuevo: UsuarioEvent
+    data object LimpiarErrorMessageNombre: UsuarioEvent
+    data object LimpiarErrorMessageBalance: UsuarioEvent
+}
