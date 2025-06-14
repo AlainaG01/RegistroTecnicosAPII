b04d4ed09fa816d9041c01fe6d5778e399780fff
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-14 10:07:45 -0400

    Screen Listar realizado

---
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/usuarios/UsuarioListScreen.kt
@@ -0,0 +1,179 @@
+package edu.ucne.registrotecnicos.presentation.usuarios
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registrotecnicos.data.remote.dto.UsuarioDto
+
+
+@Composable
+fun UsuarioListScreen(
+    viewModel: UsuarioViewModel = hiltViewModel(),
+    createUsuario: () -> Unit,
+    goToUsuario: (Int) -> Unit,
+    goBack: () -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    UsuarioListBodyScreen(
+        uiState = uiState,
+        goToUsuario = {id -> goToUsuario(id)},
+        onEvent = viewModel::onEvent,
+        createUsuario = createUsuario,
+        goBack = goBack
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
+@Composable
+fun UsuarioListBodyScreen(
+    uiState: UsuarioUiState,
+    goToUsuario: (Int) -> Unit,
+    onEvent: (UsuarioEvent) -> Unit,
+    createUsuario: () -> Unit,
+    goBack: () -> Unit
+){
+    val refreshing = uiState.isLoading
+    val pullRefreshState = rememberPullRefreshState(
+        refreshing = refreshing,
+        onRefresh = { onEvent(UsuarioEvent.GetUsuarios)}
+    )
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text("API Usuarios") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = createUsuario) {
+                Icon(Icons.Filled.Add, "Agregar nueva")
+            }
+        }
+    ){ padding ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = goBack,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "volver")
+                }
+            }
+            if (uiState.usuarios.isEmpty()){
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        text = "No hay usuarios registrados",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Gray
+                    )
+                }
+            }else {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    items(uiState.usuarios) { usuarios ->
+                        UsuariosRow(
+                            it = usuarios,
+                            goToUsuario = { goToUsuario(usuarios.usuarioId ?: 0)}
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+            }
+
+            PullRefreshIndicator(
+                refreshing = refreshing,
+                state = pullRefreshState,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+
+            if (!uiState.errorMessage.isNullOrEmpty()){
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(16.dp)
+                ){
+                    Text(
+                        text = uiState.errorMessage,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun UsuariosRow(
+    it: UsuarioDto,
+    goToUsuario: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(modifier = Modifier.weight(1f), text = it.usuarioId.toString(), color = Color.Black)
+        Text(
+            modifier = Modifier.weight(2f),
+            text = it.nombre,
+            style = MaterialTheme.typography.titleMedium,
+            color = Color.Black
+        )
+        Text(modifier = Modifier.weight(2f), text = it.balance.toString(), color = Color.Black)
+        IconButton(onClick = goToUsuario) {
+            Icon(Icons.Default.Edit, contentDescription = "Editar", tint = MaterialTheme.colorScheme.primary)
+        }
+
+    }
+    HorizontalDivider()
+}
+
