e9ab2cfb47247e4820181a93953f96260ccfc072
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-14 00:52:46 -0400

    Agregando librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.compose.material:material:1.3.1") // Usa la última versión
 
 
-
     //Hilt
     implementation("com.google.dagger:hilt-android:2.51")
     ksp("com.google.dagger:hilt-android-compiler:2.51")
