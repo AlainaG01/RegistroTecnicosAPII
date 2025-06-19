dafec389d20b6169773b5461486c6e4f782030a5
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-19 19:00:00 -0400

    Utilizando @Serializable en el dto

--- app/src/main/java/edu/ucne/registrotecnicos/data/remote/dto/UsuarioDto.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/remote/dto/UsuarioDto.kt
@@ -1,7 +1,22 @@
 package edu.ucne.registrotecnicos.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class UsuarioDto(
     val usuarioId: Int?,
     val nombre: String,
     val balance: Double
 )
+
+/*
+* En caso de que los nombres de los campos en el JSON no coinciden con los nombres de tus propiedades
+* utilizamos @SerialName ejemplo
+*
+* @Serializable
+data class UsuarioDto(
+    @SerialName("id_usuario") val usuarioId: Int?,
+    @SerialName("nombre_completo") val nombre: String,
+    val balance: Double
+)
+* */
