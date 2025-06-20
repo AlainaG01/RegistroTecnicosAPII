c85f6bb3c85497c0ec20f079620c4f7fe07e472f
alainagarcia <alaina_garcia@ucne.edu.do>
2025-06-20 01:05:42 -0400

    Agregando kotlinx.serialization

--- app/src/main/java/edu/ucne/registrotecnicos/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/di/ApiModule.kt
@@ -1,35 +1,60 @@
 package edu.ucne.registrotecnicos.di
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.registrotecnicos.data.remote.UsuarioingApi
+import kotlinx.serialization.json.Json
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
-import kotlin.jvm.java
+import okhttp3.MediaType.Companion.toMediaType
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
     const val BASE_URL = "http://www.AlainaApi.somee.com"
 
+    /*
+    // Moshi
     @Provides
     @Singleton
     fun providesMoshi(): Moshi =
         Moshi.Builder()
-            .add(KotlinJsonAdapterFactory()).add(DateAdapter())
+            .add(KotlinJsonAdapterFactory())
+            .add(DateAdapter())
             .build()
+    */
+
+    // Json parser para kotlinx.serialization
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+        encodeDefaults = false // Cambiado a false para omitir valores por defecto
+        prettyPrint = true
+    }
 
     @Provides
     @Singleton
-    fun providesUsuariosApi(moshi: Moshi): UsuarioingApi {
+    fun providesUsuariosApi(json: Json): UsuarioingApi {
+        val contentType = "application/json".toMediaType()
+
+        // (opcional) Logging interceptor para ver el JSON que se envía
+        val client = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
+            .addConverterFactory(json.asConverterFactory(contentType)) // Usando kotlinx.serialization
             .build()
             .create(UsuarioingApi::class.java)
     }
