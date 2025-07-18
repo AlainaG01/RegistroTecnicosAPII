bc128d32bcc624dd73ecbdcd605a4059ec27fe83
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-18 00:31:37 -0400

    Funciones de Vehiculo realizado

--- app/src/main/java/edu/ucne/registrotecnicos/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/remote/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package edu.ucne.registrotecnicos.data.remote
 
 import edu.ucne.registrotecnicos.data.remote.dto.UsuarioDto
+import edu.ucne.registrotecnicos.data.remote.dto.VehiculoDto
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
@@ -15,4 +16,15 @@ class RemoteDataSource @Inject constructor(
     suspend fun deleteUsuario(id: Int)= usuarioingApi.deleteUsuario(id)
 
     suspend fun getUsuario(id: Int)= usuarioingApi.getUsuario(id)
+
+    //Vehiculos
+    suspend fun getVehiculos() = usuarioingApi.getVehiculos()
+
+    suspend fun getVehiculo(id: Int) = usuarioingApi.getVehiculos(id)
+
+    suspend fun updateVehiculo(vehiculoDto: VehiculoDto) = usuarioingApi.updateVehiculo(vehiculoDto)
+
+    suspend fun saveVehiculo(vehiculoDto: VehiculoDto) = usuarioingApi.saveVehiculo(vehiculoDto)
+
+    suspend fun deleteVehiculo(idVehiculo: Int) = usuarioingApi.deleteVehiculo(idVehiculo)
 }
