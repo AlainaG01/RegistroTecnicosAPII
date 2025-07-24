e16c853233ea1740351665bfd2cd798cff3fbc0a
alainagarcia <alaina_garcia@ucne.edu.do>
2025-07-23 23:05:32 -0400

    Agregando dependencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,9 @@ dependencies {
 //    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 //    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
+    //Notificaciones
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
+
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     implementation(libs.androidx.core.ktx)
